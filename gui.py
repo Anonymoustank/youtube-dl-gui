a3d8d2e98235a665faf7b3d788b4f4fd74d2cda9
Anonymoustank <pkadekodi@gmail.com>
2020-02-17 10:53:44 -0700

    Added initial buttons

--- gui.py
+++ gui.py
@@ -1,10 +1,38 @@
 import tkinter
+from tkinter.simpledialog import askstring
 
 root = tkinter.Tk()
 
-root.title("Youtube-dl GUI")
+root.withdraw()
 
-root.geometry("600x600")
+link = askstring("Link to the video", "Enter the link to the youtube video:")
+
+def Video_Download():
+	print("hi")
+
+def Audio_Download():
+	print("hola")
+
+def initialize():
+
+	root.title("Youtube-dl GUI")
+
+	root.geometry("600x600")
+
+	root.deiconify()
+
+	video_option = tkinter.Button(root, text = "Download video", command = Video_Download)
+	
+	video_option.pack()
+
+	sound_option = tkinter.Button(root, text = "Download audio", command = Audio_Download)
+
+	sound_option.pack()
+	
 
 
+
+
+
+initialize()
 root.mainloop()
