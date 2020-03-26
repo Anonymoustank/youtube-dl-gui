1c30530d57a193419ffb722b0ada1360ce7a205e
Anonymoustank <pkadekodi@gmail.com>
2020-03-25 21:59:28 -0600

    the script doesn't use os and time anymore

--- gui.py
+++ gui.py
@@ -1,7 +1,5 @@
 from tkinter import *
 from tkinter.simpledialog import askstring
-import os
-import time
 from subprocess import PIPE, run
 
 def out(command):
@@ -15,11 +13,11 @@ def out(command):
         global error_label
 
         error_label = Label(complete,text=str(result.stderr))
-        
+
         error_label.pack()
 
         global result_label
-        
+
         result_label = Label(complete,text=str(result.stdout))
 
         result_label.pack()
@@ -33,82 +31,85 @@ def out(command):
         else:
                 error_notify = Label(complete,text="Download unsuccessful. Process exited with code " + str(result.returncode) + ".")
                 error_notify.pack()
-                
-        
-        
+
+
+
 
 def Video_Download(): #Video formats are mp4, webm
-	clicked = StringVar(root)
-	
-	clicked.set("mp4")
+        clicked = StringVar(root)
 
-	menu = OptionMenu(root, clicked, "mp4", "webm")
+        clicked.set("mp4")
 
-	menu.pack()
+        menu = OptionMenu(root, clicked, "mp4", "webm")
 
-	start_button = Button(root, text = "Start Download", command = lambda:[out("youtube-dl -q -f " + clicked.get() + " " + link + " -o " + "\"" + download + "/" + file_name + "." + clicked.get() + "\"")]) #Ask quotes to download and what comes after
+        menu.pack()
 
-	start_button.pack()
+        start_button = Button(root, text = "Start Download", command = lambda:[out("youtube-dl -q -f " + clicked.get() + " " + link + " -o " + "\"" + download + "/" + file_name + "." + clicked.get() + "\"")]) #Ask quotes to download and what comes after
 
-	back_button = Button(root, text = "Back", command = lambda:[menu.destroy(), back_button.destroy(), start_button.destroy(), initialize()])
-	
-	back_button.place(x=0,y=0)
+        start_button.pack()
+
+        back_button = Button(root, text = "Back", command = lambda:[menu.destroy(), back_button.destroy(), start_button.destroy(), initialize()])
+
+        back_button.place(x=0,y=0)
 
 def Audio_Download(): #Audio formats are mp3, m4a
-	clicked = StringVar(root)
+        clicked = StringVar(root)
 
-	clicked.set("m4a")
+        clicked.set("m4a")
 
-	menu = OptionMenu(root, clicked, "m4a", "mp3")
+        menu = OptionMenu(root, clicked, "m4a", "mp3")
 
-	menu.pack()
+        menu.pack()
 
-	start_button = Button(root, text = "Start Download", command = lambda:[out("youtube-dl -q -f " + clicked.get() + " " + link + " -o " + "\"" + download + "/" + file_name + "." + clicked.get() + "\"")]) #Add quotes to download and what comes after
+        start_button = Button(root, text = "Start Download", command = lambda:[out("youtube-dl -q -f " + clicked.get() + " " + link + " -o " + "\"" + download + "/" + file_name + "." + clicked.get() + "\"")]) #Add quotes to download and what comes after
 
-	start_button.pack()
+        start_button.pack()
 
-	back_button = Button(root, text = "Back", command = lambda:[menu.destroy(), back_button.destroy(), start_button.destroy(), initialize()])
-	
-	back_button.place(x=0,y=0)
+        back_button = Button(root, text = "Back", command = lambda:[menu.destroy(), back_button.destroy(), start_button.destroy(), initialize()])
+
+        back_button.place(x=0,y=0)
 
 def initialize():
-	root.title("Youtube-dl GUI")
+        root.title("Youtube-dl GUI")
 
-	root.geometry("600x600")
+        root.geometry("600x600")
 
-	root.deiconify()
+        root.deiconify()
 
-	video_option = Button(root, text = "Download video", command = lambda:[back_button.destroy(), sound_option.destroy(), video_option.destroy(), Video_Download()])
-	
-	video_option.pack()
+        video_option = Button(root, text = "Download video", command = lambda:[back_button.destroy(), sound_option.destroy(), video_option.destroy(), Video_Download()])
 
-	sound_option = Button(root, text = "Download audio", command = lambda:[back_button.destroy(), sound_option.destroy(), video_option.destroy(), Audio_Download()])
+        video_option.pack()
 
-	sound_option.pack()
+        sound_option = Button(root, text = "Download audio", command = lambda:[back_button.destroy(), sound_option.destroy(), video_option.destroy(), Audio_Download()])
 
-	back_button = Button(root, text = "Back", command = lambda:[root.destroy(),ask_link()])
+        sound_option.pack()
 
-	back_button.place(x=0,y=0)
+        back_button = Button(root, text = "Back", command = lambda:[root.destroy(),ask_link()])
+
+        back_button.place(x=0,y=0)
 
 def ask_link():
-	global root
-	global link
-	global download
-	global file_name
-	root = Tk()
+        global root
+        global link
+        global download
+        global file_name
+        root = Tk()
 
-	root.withdraw()
+        root.withdraw()
 
-	link = askstring("Link to the video", "Enter the link to the youtube video:")
+        link = askstring("Link to the video", "Enter the link to the youtube video:")
 
-	download = askstring("Enter your preferred download location:", "Enter your preferred download location:")
+        download = askstring("Invalid location","Enter a location that exists and you have access to")
 
-	while os.system("cd " + download) != 0:
-		download = askstring("Entered your preferred download location:", "Invalid Directory. Please enter another one:")
-	file_name = askstring("File Name? ", "File Name? ")  
-	initialize()
-	
-        
+        while str(run("cd " + download, shell=True, capture_output=True, universal_newlines=True).returncode) != "0":
+                download = askstring("Enter the video download location","Enter the video download location")
+                
+
+
+        file_name = askstring("File Name? ", "File Name? ")
+        initialize()
+
+
 
 ask_link()
 root.mainloop()
